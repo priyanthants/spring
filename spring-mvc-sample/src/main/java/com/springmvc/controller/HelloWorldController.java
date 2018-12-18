331f8fdf85c42a862812fb9d95885e38ea5499b4
priyanthan <thuraisingam.priyanthan@auxenta.com>
2018-12-18 13:07:19 +0530

    spring mvc sample application

---
+++ spring-mvc-sample/src/main/java/com/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	 @RequestMapping("/welcome")
+	    public String echo1(){
+	        return"Hello World";
+	    }
+
+}
